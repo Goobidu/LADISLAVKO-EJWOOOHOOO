2902c8199dd27e169fadabd6c74669f892844fe7
Jakub Vrtiska <1013@adc.itgymnazium.cz>
2022-12-08 10:45:54 +0100

    first commit

---
+++ LADISLAV KO6ej/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Chcete si zahrát?");
+string odpoved = Console.ReadLine();
+if (odpoved == "ano")
+{
+    Random cislojuhu = new Random();
+    int cislobuhu = cislojuhu.Next(100);
+    int count = 1;
+    for (int i = 0; i < 100; i++)
+    {
+        int odpovedalejetointprotozejetocislovisco = Convert.ToInt32(Console.ReadLine());
+        if (odpovedalejetointprotozejetocislovisco == cislobuhu)
+        {
+            Console.WriteLine("Výhra, měl jsi " + count + " pokusů");
+            break;
+        }
+        else if (odpovedalejetointprotozejetocislovisco > cislobuhu)
+        {
+            Console.WriteLine("Vygenerované číslo je menší.");
+            count++;
+        }
+        else if (odpovedalejetointprotozejetocislovisco < cislobuhu)
+        {
+            Console.WriteLine("Vygenerované číslo je větší.");
+            count++;
+        }
+        else
+        {
+            Console.WriteLine("Špatnej znak ty troubo :-(");
+        }
+    }
+}
+
+
+
